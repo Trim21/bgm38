e643e8d4c1c79bd20d45494121bb35167a3792ad
Trim21 <i@trim21.me>
2019-12-20 02:03:36 +0800

    fix: golintci

--- web/app/res/response.go
+++ web/app/res/response.go
@@ -5,10 +5,6 @@ type ValidationError struct {
 	FieldName string `json:"field_name" example:"subject_id"`
 }
 
-type Json struct {
-	Data interface{} `json:"data"`
-}
-
 type Error struct {
 	Message string `json:"message" example:"got a error in server "`
 	Status  string `json:"status" example:"error"`
