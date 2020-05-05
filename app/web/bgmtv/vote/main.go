9d8263826e979229f2951e71ac337637377452e2
Trim21 <i@trim21.me>
2020-05-05 22:27:32 +0800

    fix

--- app/web/bgmtv/vote/main.go
+++ app/web/bgmtv/vote/main.go
@@ -111,7 +111,8 @@ func render(w io.Writer, t parser.T, o Options) error {
 		}
 	}
 	if len(userVotes) == 0 {
-		c.SendString(`<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" width="1" height="1"/>`)
+		_, err := w.Write([]byte(`<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" width="1" height="1"/>`))
+		return err
 	}
 	var counter = countVotes(userVotes)
 	var v chart.Values
