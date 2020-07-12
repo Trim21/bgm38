61a1638c523edc857a677864b2bc27d82f0d3c62
Trim21 <i@trim21.me>
2020-07-12 17:50:24 +0800

    increase max subject id

--- app/cron/gen_full_url.go
+++ app/cron/gen_full_url.go
@@ -9,7 +9,7 @@ import (
 
 func genFullURL() {
 	var urls []interface{}
-	for i := 1; i < 310000; i++ {
+	for i := 1; i < 320000; i++ {
 		urls = append(urls, fmt.Sprintf("https://mirror.bgm.rin.cat/subject/%d", i))
 		if i%500 == 0 {
 			db.Redis.LPush(config.RedisSpiderURLKey, urls...)
